dc0071b0bf96b4da6c87aa01f37c4bd0fcafcf9e
Alexis Georgeson <georgeson.alexis@gmail.com>
2023-03-15 11:21:03 -0700

    Edited description and added @export

--- R/SA_step3.R
+++ R/SA_step3.R
@@ -1,9 +1,10 @@
-#' Step 3 of sensitivity analysis function 
-#' 
-#' This function computes the parameter estimates in your phantom model defined in step 1 for the different values provided. 
-#' @param step2 the object returned from SA_step2. 
-#' @param n the sample size. 
-#' SA_step3()
+#' Step 3 of sensitivity analysis function
+#'
+#' `SA_step3()` computes the parameter estimates in your phantom model defined in step 1 for the different values provided.
+#' @param step2 The object returned from SA_step2.
+#' @param n The sample size.
+#' @returns A list of parameter estimates from each test covariance matrix.
+#' @export
 
 
 SA_step3 <- function(step2,n){
@@ -12,13 +13,13 @@ SA_step3 <- function(step2,n){
   var_phant=step2[[2]]
   corlist = step2[[4]]
   covlist = step2[[5]]
-  
-  # indices of NA in matrix 
-  
+
+  # indices of NA in matrix
+
   naind <- which((is.na(matrix)&lower.tri(matrix)),arr.ind=TRUE)
-  
-  
-  
+
+
+
   sumlist <- list(NA)
   for (i in 1:nrow(combos)){
     if (corlist[[i]][2]==TRUE){
