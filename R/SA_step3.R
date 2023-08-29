8d381a0b37d917b46077dd769ad707d358bcef0d
Alexis Georgeson <ageorge@asu.edu>
2023-08-29 13:58:11 -0700

    styler applied

--- R/SA_step3.R
+++ R/SA_step3.R
@@ -10,60 +10,70 @@
 #' @examples
 #' #' @examples
 # covariance matrix
-#' covmatrix<- matrix(c(0.25, 0.95,  0.43,
-#'                      0.95, 8.87,  2.66,
-#'                      0.43, 2.66, 10.86),nrow=3, byrow=TRUE)
-#' colnames(covmatrix) <- c("X","M2","Y2")
+#' covmatrix <- matrix(c(
+#'   0.25, 0.95, 0.43,
+#'   0.95, 8.87, 2.66,
+#'   0.43, 2.66, 10.86
+#' ), nrow = 3, byrow = TRUE)
+#' colnames(covmatrix) <- c("X", "M2", "Y2")
 #'
-#'# lavann syntax for observed model
-#' observed <- ' M2 ~ X
-#'              Y2 ~ M2+X '
+#' # lavann syntax for observed model
+#' observed <- " M2 ~ X
+#'              Y2 ~ M2+X "
 #'
 #' # lavaan output
-#' obs_output <- lavaan::sem(model=observed,sample.cov=covmatrix, sample.nobs = 200)
+#' obs_output <- lavaan::sem(model = observed, sample.cov = covmatrix, sample.nobs = 200)
 #'
 #' # lavaan syntax for phantom variable model
-#' phantom <- ' M2 ~ M1 + Y1 + a*X
-#'                Y2 ~ M1 + Y1 + b*M2 + cp*X '
+#' phantom <- " M2 ~ M1 + Y1 + a*X
+#'                Y2 ~ M1 + Y1 + b*M2 + cp*X "
 #'
-#'Step1 <- SA_step1(lavoutput=obs_output,
-#'                  mod_obs = observed,
-#'                  mod_phant = phantom)
+#' Step1 <- SA_step1(
+#'   lavoutput = obs_output,
+#'   mod_obs = observed,
+#'   mod_phant = phantom
+#' )
 #'
-#' phantom_assignment <- list("CovM1X"=0,
-#'  "CovY1M1" = "CovY2M2",
-#'  "CovY1X"=0,
-#'  "VarM1"=1,
-#'  "VarY1"=1,
-#'  "CovM1M2"=seq(0,.6,.1),
-#'  "CovY1Y2"="CovM1M2",
-#'  "CovY1M2"=seq(-.6,.6,.1),
-#'  "CovM1Y2"="CovY1M2")
-#'  Step2 <- SA_step2(phantom_assignment = phantom_assignment,
-#'                   step1=Step1)
-#'  Step3 <- SA_step3(step2=Step2,
-#'                    n=200)
+#' phantom_assignment <- list(
+#'   "CovM1X" = 0,
+#'   "CovY1M1" = "CovY2M2",
+#'   "CovY1X" = 0,
+#'   "VarM1" = 1,
+#'   "VarY1" = 1,
+#'   "CovM1M2" = seq(0, .6, .1),
+#'   "CovY1Y2" = "CovM1M2",
+#'   "CovY1M2" = seq(-.6, .6, .1),
+#'   "CovM1Y2" = "CovY1M2"
+#' )
+#' Step2 <- SA_step2(
+#'   phantom_assignment = phantom_assignment,
+#'   step1 = Step1
+#' )
+#' Step3 <- SA_step3(
+#'   step2 = Step2,
+#'   n = 200
+#' )
 #'
-
-
-SA_step3 <- function(step2,n){
-  combos = step2[[3]]
-  mod_phant=step2[[1]]
-  var_phant=step2[[2]]
-  corlist = step2[[4]]
-  covlist = step2[[5]]
+SA_step3 <- function(step2, n) {
+  combos <- step2[[3]]
+  mod_phant <- step2[[1]]
+  var_phant <- step2[[2]]
+  corlist <- step2[[4]]
+  covlist <- step2[[5]]
 
   # indices of NA in matrix
 
- # naind <- which((is.na(matrix)&lower.tri(matrix)),arr.ind=TRUE)
+  # naind <- which((is.na(matrix)&lower.tri(matrix)),arr.ind=TRUE)
 
 
 
   sumlist <- list(NA)
-  for (i in 1:nrow(combos)){
-    if (corlist[[i]][2]==TRUE){
-      sumlist[[i]]=lavaan::parameterEstimates(lavaan::sem(model=mod_phant,sample.cov = covlist[[i]],sample.nobs = n))
-    } else {sumlist[[i]] = c("NPD")}
+  for (i in 1:nrow(combos)) {
+    if (corlist[[i]][2] == TRUE) {
+      sumlist[[i]] <- lavaan::parameterEstimates(lavaan::sem(model = mod_phant, sample.cov = covlist[[i]], sample.nobs = n))
+    } else {
+      sumlist[[i]] <- c("NPD")
+    }
   }
-  return(list(sumlist,corlist,covlist, combos))
+  return(list(sumlist, corlist, covlist, combos))
 }
--- data-raw/Lookup_Table.R
+++ data-raw/Lookup_Table.R
@@ -6,46 +6,45 @@ usethis::use_data(Lookup_Table, overwrite = TRUE)
 
 
 
-observedall <- expand.grid(corxm=seq(-.9,.9,.1),corxy=seq(-.9,.9,.1),cormy=seq(-.9,.9,.1))
+observedall <- expand.grid(corxm = seq(-.9, .9, .1), corxy = seq(-.9, .9, .1), cormy = seq(-.9, .9, .1))
 
 
-observedall$cond <- seq(1,nrow(observedall),1)
+observedall$cond <- seq(1, nrow(observedall), 1)
 
 
-test <-data.frame(NA)
-for (i in 1:nrow(observedall)){
-  df = matrix(c(1,observedall[i,1],observedall[i,2],observedall[i,1],1,observedall[i,3],observedall[i,2],observedall[i,3],1),nrow=3)
-  test[i,] <- is.positive.definite(df)
+test <- data.frame(NA)
+for (i in 1:nrow(observedall)) {
+  df <- matrix(c(1, observedall[i, 1], observedall[i, 2], observedall[i, 1], 1, observedall[i, 3], observedall[i, 2], observedall[i, 3], 1), nrow = 3)
+  test[i, ] <- is.positive.definite(df)
 }
 
-observedall <- cbind(observedall,test)
-observed<- observedall[which(observedall$NA.==TRUE),]
+observedall <- cbind(observedall, test)
+observed <- observedall[which(observedall$NA. == TRUE), ]
 
-results <- matrix(nrow=0,ncol=1)
-for (i in 1:nrow(observed)){
-  df = matrix(c(1,observed[i,1],observed[i,2],observed[i,1],1,observed[i,3],observed[i,2],observed[i,3],1),nrow=3)
-  temp <- cbind(cond = observed[i,4],SA_tempbias_cov(CorStability = c(-.9,.9),CorCL = c(-.9,.9),corM1Y1 = c(-.9,.9),increments = c(.1,.1,.3), covmat=df, n=300, extreme=TRUE))
-  results <- rbind(results,temp)
+results <- matrix(nrow = 0, ncol = 1)
+for (i in 1:nrow(observed)) {
+  df <- matrix(c(1, observed[i, 1], observed[i, 2], observed[i, 1], 1, observed[i, 3], observed[i, 2], observed[i, 3], 1), nrow = 3)
+  temp <- cbind(cond = observed[i, 4], SA_tempbias_cov(CorStability = c(-.9, .9), CorCL = c(-.9, .9), corM1Y1 = c(-.9, .9), increments = c(.1, .1, .3), covmat = df, n = 300, extreme = TRUE))
+  results <- rbind(results, temp)
 }
 
-#remove NA
-results_noNA <- results %>% filter(jointsigab=="Yes"|jointsigab=="No")
+# remove NA
+results_noNA <- results %>% filter(jointsigab == "Yes" | jointsigab == "No")
 setwd("G:/My Drive/ASU/Dave lab/temporal bias/Manuscript/Revision/Lookup Table")
-write.csv(results,"./lookup_table_all.csv")
-write_rds(results,"./lookup_table_all.rds")
+write.csv(results, "./lookup_table_all.csv")
+write_rds(results, "./lookup_table_all.rds")
 
 
 
-write.csv(results_noNA,"./lookup_table_noNA.csv")
-write_rds(results_noNA,"./lookup_table_noNA.rds")
+write.csv(results_noNA, "./lookup_table_noNA.csv")
+write_rds(results_noNA, "./lookup_table_noNA.rds")
 
 # remove empty columns
-obs2 <- observed[,1:4]
-write_rds(obs2,"./conditions.rds")
+obs2 <- observed[, 1:4]
+write_rds(obs2, "./conditions.rds")
 
-#this puts it in the package
-usethis::use_data(results_noNA,obs2,internal=TRUE)
+# this puts it in the package
+usethis::use_data(results_noNA, obs2, internal = TRUE)
 
-#creates data-raw
-#usethis::use_data_raw("Lookup_Table")
-
+# creates data-raw
+# usethis::use_data_raw("Lookup_Table")
