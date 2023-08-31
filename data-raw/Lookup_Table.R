2e0a78970723c9352205a70bb39b173ce8eff870
Alexis Georgeson <ageorge@asu.edu>
2023-08-31 11:15:14 -0700

    - reduced size of lookup table for cran

--- data-raw/Lookup_Table.R
+++ data-raw/Lookup_Table.R
@@ -15,7 +15,7 @@ observedall$cond <- seq(1, nrow(observedall), 1)
 test <- data.frame(NA)
 for (i in 1:nrow(observedall)) {
   df <- matrix(c(1, observedall[i, 1], observedall[i, 2], observedall[i, 1], 1, observedall[i, 3], observedall[i, 2], observedall[i, 3], 1), nrow = 3)
-  test[i, ] <- is.positive.definite(df)
+  test[i, ] <- corpcor::is.positive.definite(df)
 }
 
 observedall <- cbind(observedall, test)
@@ -43,8 +43,23 @@ write_rds(results_noNA, "./lookup_table_noNA.rds")
 obs2 <- observed[, 1:4]
 write_rds(obs2, "./conditions.rds")
 
+
+results_noNA <- readRDS("G:/My Drive/ASU/Dave lab/temporal bias/Manuscript/Revision/Lookup Table/lookup_table_noNA_saverds.rds")
+
+
+#saveRDS(results_smaller,"G:/My Drive/ASU/Dave lab/temporal bias/Manuscript/Revision/Lookup Table/lookup_table_noNA_small.rds",compress="xz")
+#saveRDS(results_noNA,"G:/My Drive/ASU/Dave lab/temporal bias/Manuscript/Revision/Lookup Table/lookup_table_noNA_saverds.rds",compress="xz")
+
+
+results_smaller <- results_noNA[,c(1:5,8:9,14)]
+
+
+results_smaller <- readRDS("G:/My Drive/ASU/Dave lab/temporal bias/Manuscript/Revision/Lookup Table/lookup_table_noNA_small.rds")
+
 # this puts it in the package
-usethis::use_data(results_noNA, obs2, internal = TRUE)
+usethis::use_data(results_smaller, obs2, internal = TRUE, overwrite=TRUE, compress = "xz")
+
+
 
 # creates data-raw
 # usethis::use_data_raw("Lookup_Table")
